0e147fb425807bc470a3a2e6114d40ab237d5376
LuLuLin <764613693@qq.com>
2022-12-07 21:39:35 +0800

    人资项目初始化_肃清模板之后

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+export default service
