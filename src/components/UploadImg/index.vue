2784774b3fe0870b57f59f5cab6568cdc335e5d3
林伟源 <100758348+yinhemade@users.noreply.github.com>
2023-01-05 02:36:01 +0800

    已将该密匙作废

--- src/components/UploadImg/index.vue
+++ src/components/UploadImg/index.vue
@@ -18,8 +18,8 @@ var COS = require('cos-js-sdk-v5')
 
 // SECRETID 和 SECRETKEY请登录 https://console.cloud.tencent.com/cam/capi 进行查看和管理
 var cos = new COS({
-  SecretId: 'AKIDNacR5asxsa5NPNAjOnz66speIoJhspXy',
-  SecretKey: 'QabHogV9Wu48ZpqcCamjS1LVR8g6LM8f'
+  SecretId: '',
+  SecretKey: ''
 })
 export default {
   name: 'UploadImg',
